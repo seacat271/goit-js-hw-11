ea4d467fabd7b909de95c596d28f4ab4032dff44
seacat271 <seacat271083@gmail.com>
2022-08-24 13:24:23 +0300

    fix

--- src/js-modules/APIGallery.js
+++ src/js-modules/APIGallery.js
@@ -8,21 +8,20 @@ export default class PixabayGallery {
     constructor() {
         this.galleryQuery = "";
         this.page = 1;
+
     }
 
     requestConfig = {
         params: {
         key: "29127762-27ecb80fc89c6fc72c273a026",
         per_page: 40,
-        page: this.page,
-        q: this.galleryQuery,
     }}
 
     async getFetchImages(){
         const BASE_URL = "https://pixabay.com/api/"
-
+       
         try {
-            const response = await axios.get(`${BASE_URL}`, this.requestConfig)
+            const response = await axios.get(`${BASE_URL}?page=${this.page}&q=${this.galleryQuery}`, this.requestConfig)
             if(response.status === 200) this.page += 1;
             return response.data
         } catch {
