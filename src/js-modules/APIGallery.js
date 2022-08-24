6766a8577c45fae618cbfc4e25147e4fcc580339
seacat271 <seacat271083@gmail.com>
2022-08-24 13:09:52 +0300

    add axios

--- src/js-modules/APIGallery.js
+++ src/js-modules/APIGallery.js
@@ -1,21 +1,37 @@
+import axios from "axios";
+
+
+
+
+
 export default class PixabayGallery {
     constructor() {
         this.galleryQuery = "";
         this.page = 1;
     }
+
+    requestConfig = {
+        params: {
+        key: "29127762-27ecb80fc89c6fc72c273a026",
+        per_page: 40,
+        page: this.page,
+        q: this.galleryQuery,
+    }}
+
     async getFetchImages(){
         const BASE_URL = "https://pixabay.com/api/"
-        const API_KEY = "29127762-27ecb80fc89c6fc72c273a026"
+
         try {
-            const response = await fetch(`${BASE_URL}?key=${API_KEY}&q=${this.galleryQuery}&per_page=40&page=${this.page}`)
-            if(response.ok) this.page += 1;
-            const json = response.json()
-            return json
+            const response = await axios.get(`${BASE_URL}`, this.requestConfig)
+            if(response.status === 200) this.page += 1;
+            return response.data
         } catch {
             showErrorMessage()
             return json = {};
-        }
-}
+        }   
+    }
+
+
 
     get query() {
         return this.galleryQuery
